868101178d285f83d38bd6d158f4768085be5b96
AndrewJamesTurner <turner.andrew.1990@gmail.com>
2014-05-28 17:04:52 +0100

    Added.cpp and .h files

---
+++ MPU60X0.cpp
@@ -0,0 +1,328 @@
+
+/*
+  MPU60X0.cpp - Library for interfacing with MPU60X0 chip.
+  Created by Andrew James Turner, May 25, 2014.
+  Released into the public domain.
+*/
+
+#include "Arduino.h"
+#include <MPU60X0.h>
+#include <Wire.h> 
+
+
+MPU60X0::MPU60X0(void)
+{
+	Wire.begin();
+
+	wakeUp();
+	
+	_accelRange = getData(ACCEL_CONFIG);
+	_accelRange = _accelRange & 0b00011000;
+
+	_gyroRange = getData(GYRO_CONFIG);
+	_gyroRange = _gyroRange & 0b00011000;
+}
+
+
+int MPU60X0::wakeUp(void){
+  
+	uint8_t sleepBit;
+	const uint8_t SLEEP = 0;
+  
+	// set sleep bit to zero
+	Wire.beginTransmission(device_address);
+	Wire.write(PWR_MGMT_1);
+	Wire.write(&SLEEP, 1);
+	Wire.endTransmission(true);
+
+	// get the sleep bit
+	Wire.requestFrom(device_address, 1, true);
+	sleepBit = Wire.read();
+ 
+	// check to see if woke up
+	if(sleepBit == 0){
+		return 1;
+	}
+	else{
+		return 0;
+	} 
+}
+
+
+/*
+  fetches one byte of data from the given register
+*/
+uint8_t MPU60X0::getData(int reg){
+  
+  uint8_t data;
+   
+  Wire.beginTransmission(device_address);
+  Wire.write(reg);
+  Wire.endTransmission(false);
+
+  Wire.requestFrom(device_address, 1, true);
+   
+  data =  Wire.read();
+   
+  return data;
+}
+
+
+float MPU60X0::getAccelX(void){
+  
+	uint8_t lowerByte;
+	uint8_t upperByte; 
+	int16_t accel;
+	  
+	lowerByte = getData(ACCEL_XOUT_L);
+	upperByte = getData(ACCEL_XOUT_H);
+
+	accel = (int16_t)upperByte;
+	accel <<= 8;
+	accel |= lowerByte;
+
+	if(_accelRange == 0)
+		return (float)accel / 16384;
+	else if(_accelRange == 1)
+		return (float)accel / 8192;
+	else if(_accelRange == 2)
+		return (float)accel / 4096;
+	else
+		return (float)accel / 2048;
+}
+
+float MPU60X0::getAccelY(void){
+  
+	uint8_t lowerByte;
+	uint8_t upperByte; 
+	int16_t accel;
+
+	lowerByte = getData(ACCEL_YOUT_L);
+	upperByte = getData(ACCEL_YOUT_H);
+
+	accel = (int16_t)upperByte;
+	accel <<= 8;
+	accel |= lowerByte;
+
+	if(_accelRange == 0)
+		return (float)accel / 16384;
+	if(_accelRange == 1)
+		return (float)accel / 8192;
+	if(_accelRange == 2)
+		return (float)accel / 4096;
+	else
+		return (float)accel / 2048;
+}
+
+
+float MPU60X0::getAccelZ(void){
+  
+	uint8_t lowerByte;
+	uint8_t upperByte; 
+	int16_t accel;
+
+	lowerByte = getData(ACCEL_ZOUT_L);
+	upperByte = getData(ACCEL_ZOUT_H);
+
+	accel = (int16_t)upperByte;
+	accel <<= 8;
+	accel |= lowerByte;
+
+	if(_accelRange == 0)
+		return (float)accel / 16384;
+	if(_accelRange == 1)
+		return (float)accel / 8192;
+	if(_accelRange == 2)
+		return (float)accel / 4096;
+	else
+		return (float)accel / 2048;
+}
+
+float MPU60X0::getGyroX(void){
+  
+	uint8_t lowerByte;
+	uint8_t upperByte; 
+	int16_t gyro;
+
+	lowerByte = getData(GYRO_XOUT_L);
+	upperByte = getData(GYRO_XOUT_H);
+
+	gyro = (int16_t)upperByte;
+	gyro <<= 8;
+	gyro |= lowerByte;
+  
+	if(_gyroRange == 0)
+		return (float)gyro / 131;
+	if(_gyroRange == 1)
+		return (float)gyro / 65.5;
+	if(_gyroRange == 2)
+		return (float)gyro / 32.8;
+	else
+		return (float)gyro / 16.4;
+}
+
+
+float MPU60X0::getGyroY(void){
+  
+	uint8_t lowerByte;
+	uint8_t upperByte; 
+	int16_t gyro;
+
+	lowerByte = getData(GYRO_YOUT_L);
+	upperByte = getData(GYRO_YOUT_H);
+
+	gyro = (int16_t)upperByte;
+	gyro <<= 8;
+	gyro |= lowerByte;
+  
+	if(_gyroRange == 0)
+		return (float)gyro / 131;
+	if(_gyroRange == 1)
+		return (float)gyro / 65.5;
+	if(_gyroRange == 2)
+		return (float)gyro / 32.8;
+	else
+		return (float)gyro / 16.4;
+}
+
+float MPU60X0::getGyroZ(void){
+  
+	uint8_t lowerByte;
+	uint8_t upperByte; 
+	int16_t gyro;
+
+	lowerByte = getData(GYRO_ZOUT_L);
+	upperByte = getData(GYRO_ZOUT_H);
+
+	gyro = (int16_t)upperByte;
+	gyro <<= 8;
+	gyro |= lowerByte;
+  
+	if(_gyroRange == 0)
+		return (float)gyro / 131;
+	if(_gyroRange == 1)
+		return (float)gyro / 65.5;
+	if(_gyroRange == 2)
+		return (float)gyro / 32.8;
+	else
+		return (float)gyro / 16.4;
+}
+
+
+float MPU60X0::getTemp(void){
+  
+  uint8_t lowerByte;
+  uint8_t upperByte;
+  int16_t temp;
+    
+  lowerByte = getData(TEMP_OUT_L);
+  upperByte = getData(TEMP_OUT_H);
+
+  temp = (int16_t)upperByte;
+  temp <<= 8;
+  temp |= lowerByte;
+  
+  return ((float)temp/340) + 36.53;
+}
+
+
+void MPU60X0::setAccelRange2G(void){
+  
+	uint8_t range = 0b00000000;
+  
+	setData(ACCEL_CONFIG, range);
+  
+	_accelRange = getData(ACCEL_CONFIG);
+	_accelRange = _accelRange & 0b00011000;
+}
+
+void MPU60X0::setAccelRange4G(void){
+  
+	uint8_t range = 0b00001000;
+  
+	setData(ACCEL_CONFIG, range);
+  
+	_accelRange = getData(ACCEL_CONFIG);
+	_accelRange = _accelRange & 0b00011000;
+}
+
+void MPU60X0::setAccelRange8G(void){
+  
+	uint8_t range = 0b00010000;
+  
+	setData(ACCEL_CONFIG, range);
+  
+	_accelRange = getData(ACCEL_CONFIG);
+	_accelRange = _accelRange & 0b00011000;
+}
+
+
+
+void MPU60X0::setAccelRange16G(void){
+  
+	uint8_t range = 0b00011000;
+  
+	setData(ACCEL_CONFIG, range);
+  
+	_accelRange = getData(ACCEL_CONFIG);
+	_accelRange = _accelRange & 0b00011000;
+}
+
+
+void MPU60X0::setGyroRange250DegPerSec(void){
+  
+	uint8_t range = 0b00000000;
+  
+	setData(ACCEL_CONFIG, range);
+  
+	_gyroRange = getData(GYRO_CONFIG);
+	_gyroRange = _gyroRange & 0b00011000;
+}
+
+void MPU60X0::setGyroRange500DegPerSec(void){
+  
+	uint8_t range = 0b00001000;
+  
+	setData(ACCEL_CONFIG, range);
+  
+	_gyroRange = getData(GYRO_CONFIG);
+	_gyroRange = _gyroRange & 0b00011000;
+}
+
+
+void MPU60X0::setGyroRange1000DegPerSec(void){
+  
+	uint8_t data = 0b00010000;
+  
+	setData(ACCEL_CONFIG, data);
+  
+	_gyroRange = getData(GYRO_CONFIG);
+	_gyroRange = _gyroRange & 0b00011000;
+}
+
+
+void MPU60X0::setGyroRange2000DegPerSec(void){
+  
+	uint8_t range = 0b00011000;
+  
+	setData(ACCEL_CONFIG, range);
+  
+	_gyroRange = getData(GYRO_CONFIG);
+	_gyroRange = _gyroRange & 0b00011000;
+}
+
+
+/*
+  fetches one byte of data from the given register
+*/
+void MPU60X0::setData(int reg, const uint8_t data){
+  
+  Wire.beginTransmission(device_address);
+  Wire.write(reg);
+  Wire.write(&data, 1);
+  Wire.endTransmission(true);
+}
+
+
+
+
---
+++ MPU60X0.h
@@ -0,0 +1,61 @@
+#ifndef MPU60X0_h
+#define MPU60X0_h
+
+	#include <math.h>
+	#include "Arduino.h"
+
+	#define device_address 	0x68 
+
+	#define ACCEL_CONFIG 	0x1C
+	#define GYRO_CONFIG 	0x1B
+	
+	#define ACCEL_XOUT_H  	0x3B
+	#define ACCEL_XOUT_L  	0x3C
+	#define ACCEL_YOUT_H  	0x3D
+	#define ACCEL_YOUT_L  	0x3E
+	#define ACCEL_ZOUT_H  	0x3F
+	#define ACCEL_ZOUT_L  	0x40
+
+	#define TEMP_OUT_H  	0x41
+	#define TEMP_OUT_L  	0x42
+
+	#define GYRO_XOUT_H 	0x43
+	#define GYRO_XOUT_L 	0x44
+	#define GYRO_YOUT_H 	0x45
+	#define GYRO_YOUT_L 	0x46
+	#define GYRO_ZOUT_H 	0x47
+	#define GYRO_ZOUT_L 	0x48
+
+	#define PWR_MGMT_1 		0x6B 
+
+	class MPU60X0
+	{
+		public:
+			MPU60X0(void);
+			float getAccelX(void);
+			float getAccelY(void);
+			float getAccelZ(void);
+			float getGyroX(void);
+			float getGyroY(void);
+			float getGyroZ(void);
+			float getTemp(void);
+
+			void setAccelRange2G(void);
+			void setAccelRange4G(void);
+			void setAccelRange8G(void);
+			void setAccelRange16G(void);
+
+			void setGyroRange250DegPerSec(void);
+			void setGyroRange500DegPerSec(void);
+			void setGyroRange1000DegPerSec(void);
+			void setGyroRange2000DegPerSec(void);
+
+		private:
+			int _accelRange;
+			int _gyroRange;
+			int wakeUp(void);
+			uint8_t getData(int reg);
+			void setData(int reg, const uint8_t data);
+	};
+
+#endif
