c45069317a5e85cd28074696a7b72dc16a2d7ed0
AndrewJamesTurner <turner.andrew.1990@gmail.com>
2014-05-28 18:03:02 +0100

    Example now works

--- examples/displayStatus/displayStatus.ino
+++ examples/displayStatus/displayStatus.ino
@@ -6,10 +6,15 @@ MPU6050 mpu6050;
 void setup()
 {   
   Serial.begin(9600);
+
+    mpu6050.begin();
 }
 
 
 void loop()
 {
+   Serial.print("Acell X: ");
    Serial.println(mpu6050.getAccelX());
+
+    delay(500);
 }
